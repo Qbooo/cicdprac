2c12c1394144d347bc52738f4f57d2687f2790b0
SyaDo95 <70837945+SyaDo95@users.noreply.github.com>
2025-07-31 00:01:23 +0900

    Fix: ã: hihi

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello, 규보야! ByeBye";
+        return "Hello 하이하이, 규보야! ByeBye";
     }
     //dkdk
 }
