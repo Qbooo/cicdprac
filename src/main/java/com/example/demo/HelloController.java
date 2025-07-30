a1f975381a1313dd874ae1b3d4dce2f62b86dc99
SyaDo95 <70837945+SyaDo95@users.noreply.github.com>
2025-07-30 23:28:48 +0900

    Bye

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello, 규보야!";
+        return "Hello, 규보야! ByeBye";
     }
     //dkdk
 }
